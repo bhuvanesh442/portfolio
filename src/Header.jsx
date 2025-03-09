9001df9a19a13e7de4615b74b54c2287cdd866bd
bhuvanesh442 <bhuvanesh02052005@gmail.com>
2025-03-09 12:18:08 +0530

    Profile Popup

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import Profile from "./Profile";
+import React, { useState } from "react";
+import ProfilePopup from "./ProfilePopup";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
+  <>
     <div className="w-full flex items-center justify-between p-1 border-b-3 border-gray-700 sticky top-0 bg-gray-900 z-10 ">
       <div className="flex flex-col items-center flex-grow">
         <h1 className="text-4xl font-bold text-center">Bhuvanesh B</h1>
@@ -12,7 +14,10 @@ export default function Header() {
         src="/bhuvanesh.JPG"
         alt="Bhuvanesh B"
         className="w-16 h-16 rounded-full border-3 border-gray-700"
+        onClick={() => setIsOpen(true)}
       />
     </div>
+     <ProfilePopup isOpen={isOpen} onClose={() => setIsOpen(false)} />
+  </>
   );
 }
---
+++ src/ProfilePopup.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Footer from "./Footer";
+
+export default ProfilePopup = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+  return (
+    <div className="fixed inset-0 flex items-center justify-center border border-gray-800 shadow-2xl p-4 ">
+      <div className="bg-gray-800 p-6 rounded-xl shadow-lg w-96 text-center relative  border border-gray-900 ">
+        <button
+          onClick={onClose}
+          className="absolute top-3 right-3 text-white text-2xl"
+        >
+          &times;
+        </button>
+
+        <img
+          src="/bhuvanesh.JPG"
+          alt="Bhuvanesh B"
+          className="w-32 h-32 rounded-2xl mx-auto border-2 border-gray-500"
+        />
+        <h2 className="text-2xl font-bold mt-3">Bhuvanesh B</h2>
+        <p className="text-gray-400">B.Pharm | MERN Stack Developer</p>
+
+        <p className="mt-3 text-gray-300">
+          Passionate about programming and web development.
+        </p>
+
+        <Footer />
+      </div>
+    </div>
+  );
+};
